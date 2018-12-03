b147ace755739181bfe0a7823ae3a1937aa5ec4f
lytze <liyutze.seikiri@gmail.com>
2018-12-03 17:14:15 +0800

    use external font link

--- shiny/ui.R
+++ shiny/ui.R
@@ -3,7 +3,10 @@ shinyUI(
     fluidPage(
         tags$head(
             tags$title('ANN for Protein Secondary Structure Prediction'),
-            tags$link(rel = "stylesheet", href = "lib/font-awesome/css/font-awesome.min.css"),
+            tags$link(rel = "stylesheet",
+                      href = "https://use.fontawesome.com/releases/v5.5.0/css/all.css",
+                      integrity = "sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU",
+                      crossorigin="anonymous"),
             tags$script(type = 'text/javascript', src = 'custom.js'),
             tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
         ),
